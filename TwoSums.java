3d7de2be979f82156115755c0d86b633246d8e5a
expert26111 <djoana.v@gmail.com>
2022-11-13 21:36:01 +0100

    adding code for using hashmap with one loop

--- TwoSums.java
+++ TwoSums.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class TwoSums 
 {
    
@@ -11,8 +14,20 @@ public class TwoSums
 
           int[] nums3 = {3,3};
           int target3 = 6;
-
+ 
           int[] indexes = twoSum(nums,target);
+         
+          if(indexes != null)
+          {
+            System.out.printf("The indexes are:  [%d,%d] !%n", indexes[0],indexes[1]);
+          }
+          indexes = twoSums_with_HashMap(nums,target);
+          if(indexes != null)
+          {
+            System.out.printf("The indexes are:  [%d,%d] !%n", indexes[0],indexes[1]);
+          }
+        
+          indexes = twoSums_with_HashMap_One_For_Loop(nums,target);
           if(indexes != null)
           {
             System.out.printf("The indexes are:  [%d,%d] !%n", indexes[0],indexes[1]);
@@ -24,12 +39,34 @@ public class TwoSums
           {
             System.out.printf("The indexes are:  [%d,%d] !%n", indexes2[0],indexes2[1]);
           }
+          indexes2 = twoSums_with_HashMap(nums2,target2);
+          if(indexes2 != null)
+          {
+            System.out.printf("The indexes are:  [%d,%d] !%n", indexes2[0],indexes2[1]);
+          }
+          indexes2 = twoSums_with_HashMap_One_For_Loop(nums2,target2);
+          if(indexes2 != null)
+          {
+            System.out.printf("The indexes are:  [%d,%d] !%n", indexes2[0],indexes2[1]);
+          }
+
 
           int[] indexes3 = twoSum(nums3,target3);
           if(indexes3 != null)
           {
             System.out.printf("The indexes are:  [%d,%d] !%n", indexes3[0],indexes3[1]);
           }
+          indexes3 = twoSums_with_HashMap(nums3,target3);
+          if(indexes3 != null)
+          {
+            System.out.printf("The indexes are:  [%d,%d] !%n", indexes3[0],indexes3[1]);
+          }
+          indexes3 = twoSums_with_HashMap_One_For_Loop(nums3,target3);
+          if(indexes3 != null)
+          {
+            System.out.printf("The indexes are:  [%d,%d] !%n", indexes3[0],indexes3[1]);
+          }
+ 
     }
 
     public static int[] twoSum(int[] nums, int target) 
@@ -48,6 +85,41 @@ public class TwoSums
         }
         return null;
     }
+
+    public static int[] twoSums_with_HashMap(int[] nums, int target)
+    {
+            Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+            for ( int index = 0; index <= nums.length-1;index++)
+            {
+                 map.put(nums[index],index);
+            }   
+            for ( int index = 0; index <= nums.length-1;index++ )
+            {
+                int complement = target - nums[index];
+                if( map.containsKey(complement) &&  map.get(complement) != index)
+                {
+                    return new int[] { index,map.get(complement) };
+                }
+            }
+
+            return null;
+    }
+
+    public static int[] twoSums_with_HashMap_One_For_Loop(int[] nums, int target)
+    {
+            Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+            for ( int index = 0; index <= nums.length-1;index++)
+            {
+                 int complement = target - nums[index];
+                 if(map.containsKey(complement))
+                 {
+                    return new int[] { map.get(complement),index };
+                 }
+                 map.put(nums[index],index);
+            }   
+            return null;
+    }
+
 
 
 }
